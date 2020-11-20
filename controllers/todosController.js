c346642b1afa223e28c6ba38018232eafc152fc4
RazanOmar <razanosiri@gmail.com>
2020-11-20 19:05:38 +0200

    Todos

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -2,7 +2,8 @@ const todosmodel=require('../models/todos');
 
 /////Insert
 module.exports.insert= async(req,res)=>{
-    try{    const {todo,membername,memberemail}=req.body;
+    try{    
+    const {todo,membername,memberemail}=req.body;
     const todos=new todosmodel({
         todo,membername,memberemail
     });
@@ -14,3 +15,100 @@ module.exports.insert= async(req,res)=>{
     console.log('error in insertion'+e);
 }
 }
+////////Select all todos
+module.exports.show=async(req,res)=>{
+    try{
+        const todos=await todosmodel.find();
+        console.log(todos);
+    }catch(e)
+    {
+        console.log(e);
+    }
+}
+///////Select a specific todo
+module.exports.selectid=async(req,res)=>{
+    try{
+        const {id}=req.body;
+        //find by id 
+        const todos=await todosmodel.findById(id);
+         if(!todos) console.log("Not Found!");
+         else{
+        console.log(todos);
+         }
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+////////Update Todo
+module.exports.update=async(req,res)=>{
+    try{
+        const {id,todo,membername,memberemail}=req.body;
+
+        //find by id
+        const todos=await todosmodel.findById(id);
+        if(!todos) return;
+        //modify properties
+        todos.set({
+            todo:todo,
+            membername:membername,
+            memberemail:memberemail
+    })
+        //save
+        const result=await todos.save();
+        console.log("Updated");
+        }
+    catch(e){
+        console.log(e)
+    }
+}
+///////Delete Todo
+module.exports.delete=async(req,res)=>{
+    try{
+        const {id}=req.body;
+        //find by id and delete
+        const todo=await todosmodel.findByIdAndRemove(id);
+        console.log("Deleted");
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+///////Delete todos
+module.exports.deletemany=async(req,res)=>{
+    try{
+        const todo=await todosmodel.deleteMany();
+        console.log(todo);
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
+}
+//////Search Todo
+module.exports.search=async(req,res)=>{
+    try{
+        const {id}=req.body;
+        //find by id and return 
+        const todo=await todosmodel
+        .findById(id)
+        .select({membername:1 ,memberemail:1});
+        console.log(todo);
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,23 @@ const todoscontroller=require('../controllers/todosController');
 router.get('/', function(req, res,) {
     res.render('todos', { layout: 'main' });
   });
-  
+  router.get('/selectid', function(req, res,) {
+    res.render('view', { layout: 'main' });
+  });
+  router.get('/update', function(req, res,) {
+    res.render('update', { layout: 'main' });
+  });
+  router.get('/delete', function(req, res,) {
+    res.render('delete', { layout: 'main' });
+  });
+  router.get('/search', function(req, res,) {
+    res.render('search', { layout: 'main' });
+  });
 router.post('/todos',todoscontroller.insert);
+router.get('/show',todoscontroller.show);
+router.post('/selectid',todoscontroller.selectid);
+router.post('/update',todoscontroller.update);
+router.post('/delete',todoscontroller.delete);
+router.get('/deletemany',todoscontroller.deletemany);
+router.post('/search',todoscontroller.search);
 module.exports = router;
